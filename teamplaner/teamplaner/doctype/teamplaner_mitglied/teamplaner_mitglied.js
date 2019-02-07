422b56e7b2496d3ec4521dea731ce4b4cf21219f
Joelios <joel.mesmer@libracore.com>
2019-02-07 15:45:55 +0100

    added new feature (invite user) and implemented new role management

--- teamplaner/teamplaner/doctype/teamplaner_mitglied/teamplaner_mitglied.js
+++ teamplaner/teamplaner/doctype/teamplaner_mitglied/teamplaner_mitglied.js
@@ -3,6 +3,18 @@
 
 frappe.ui.form.on('TeamPlaner Mitglied', {
 	refresh: function(frm) {
-
+		if(!frm.is_new() && frm.perm[0].write) {
+			frm.add_custom_button(__("Invite as User"), function() {
+				return frappe.call({
+					method: "teamplaner.teamplaner.doctype.teamplaner_mitglied.teamplaner_mitglied.invite_user",
+					args: {
+						mitglied: frm.doc.name
+					},
+					callback: function(r) {
+						frappe.msgprint("Es wurde erfolgreich ein User-Account eingerichtet.");
+					}
+				});
+			});
+		}
 	}
 });
--- teamplaner/teamplaner/doctype/teamplaner_mitglied/teamplaner_mitglied.py
+++ teamplaner/teamplaner/doctype/teamplaner_mitglied/teamplaner_mitglied.py
@@ -8,3 +8,22 @@ from frappe.model.document import Document
 
 class TeamPlanerMitglied(Document):
 	pass
+
+@frappe.whitelist()
+def invite_user(mitglied):
+	spieler = frappe.get_doc("TeamPlaner Mitglied", mitglied)
+
+	if not spieler.mail:
+		frappe.throw(_("Please set Email Address"))
+
+	if spieler.has_permission("write"):
+		user = frappe.get_doc({
+			"doctype": "User",
+			"first_name": spieler.vorname,
+			"last_name": spieler.nachname,
+			"email": spieler.mail,
+			"user_type": "Website User",
+			"send_welcome_email": 1
+		}).insert(ignore_permissions = True)
+
+	return user.name
