d47bae5c9f5e2bac3c1313953267175a45ad2d57
Joelios <joel.mesmer@libracore.com>
2019-02-12 23:52:27 +0100

    #26

--- teamplaner/www/anwesenheitskontrolle.js
+++ teamplaner/www/anwesenheitskontrolle.js
@@ -53,6 +53,47 @@ function busse(training, spieler, anmeldung, tr) {
 	}
 }
 
+function change_anwesenheit(training, spieler, anmeldung, tr) {	
+	//frappe.msgprint(spieler);
+	if (tr.style.backgroundColor == "red") {
+		frappe.call({
+			method: "teamplaner.utils.change_anwesenheit",
+			args:{
+				'training': training,
+				'spieler': spieler,
+				'status': "Abwesend"
+			},
+			callback: function(r)
+			{
+				if (tr.style.backgroundColor == "red") {
+					tr.style.backgroundColor = "#50D050";
+				} else {
+					tr.style.backgroundColor = "red";
+				}
+			}
+		});
+
+	} else {
+		frappe.call({
+			method: "teamplaner.utils.change_anwesenheit",
+			args:{
+				'training': training,
+				'spieler': spieler,
+				'status': "Anwesend"
+			},
+			callback: function(r)
+			{
+				if (tr.style.backgroundColor == "red") {
+					tr.style.backgroundColor = "#50D050";
+				} else {
+					tr.style.backgroundColor = "red";
+				}
+			}
+		});
+
+	}
+}
+
 function alle_trainings_laden() {
 	var acc = document.getElementById("accordion_kurz");
 	var btn = document.getElementById("show_all_btn");
