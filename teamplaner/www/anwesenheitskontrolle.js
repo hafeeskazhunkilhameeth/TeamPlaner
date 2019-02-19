4f112085fdebe022d1b6bd8296afe2c1dabf893e
Joelios <joel.mesmer@libracore.com>
2019-02-19 15:23:26 +0100

    #38

--- teamplaner/www/anwesenheitskontrolle.js
+++ teamplaner/www/anwesenheitskontrolle.js
@@ -1,4 +1,5 @@
-function busse(training, spieler, anmeldung, tr) {	
+/* function busse(training, spieler, anmeldung, tr) {	
+	var tr = document.getElementById(tr);
 	if (tr.style.backgroundColor == "red") {
 		frappe.call({
 			method: "teamplaner.utils.change_anwesenheit",
@@ -25,6 +26,7 @@ function busse(training, spieler, anmeldung, tr) {
 			}
 		});
 	} else {
+		
 		frappe.call({
 			method: "teamplaner.utils.change_anwesenheit",
 			args:{
@@ -51,8 +53,72 @@ function busse(training, spieler, anmeldung, tr) {
 			}
 		});
 	}
+} */
+
+function ohne_busse(training, spieler, anmeldung, tr) {	
+	var tr = document.getElementById(tr);
+	frappe.call({
+		method: "teamplaner.utils.change_anwesenheit",
+		args:{
+			'training': training,
+			'spieler': spieler,
+			'status': "Abwesend"
+		},
+		callback: function(r)
+		{
+			
+			if (tr.style.backgroundColor == "red") {
+				tr.style.backgroundColor = "#50D050";
+			} else {
+				tr.style.backgroundColor = "red";
+			}
+			
+			if (r.message == "keine_busse") {
+				frappe.msgprint("Status geändert");
+			} else {
+				frappe.msgprint("Status geändert und Busse ausgelöst");
+			}
+			
+		}
+	});
 }
 
+function mit_busse(training, spieler, anmeldung, tr) {	
+	var tr = document.getElementById(tr);
+	frappe.call({
+		method: "teamplaner.utils.change_anwesenheit",
+		args:{
+			'training': training,
+			'spieler': spieler,
+			'status': "Anwesend",
+			'busse': 'ja'
+		},
+		callback: function(r)
+		{
+			
+			if (tr.style.backgroundColor == "red") {
+				tr.style.backgroundColor = "#50D050";
+			} else {
+				tr.style.backgroundColor = "red";
+			}
+			
+			if (r.message == "keine_busse") {
+				frappe.msgprint("Status geändert");
+			} else {
+				frappe.msgprint("Status geändert und Busse ausgelöst");
+			}
+			
+		}
+	});
+}
+function abfrage(training, spieler, anmeldung, tr) {
+	var busse_btn_1 = '<center><button style="width: 95px;" class="btn btn-primary" onclick="mit_busse('
+	var argumente = "'" + training + "', '" + spieler + "', '" + anmeldung + "', '" + tr + "'";
+	var busse_btn_2 = ');">Mit Busse</button>';
+	var busse_btn_3 = '<button style="width: 95px;" class="btn btn-primary" onclick="ohne_busse('
+	var busse_btn_4 = ');">Ohne Busse</button></center>';
+	frappe.msgprint(busse_btn_1 + argumente + busse_btn_2 + "<br><br>" + busse_btn_3 + argumente + busse_btn_4, "Bussen-Management");
+}
 function change_anwesenheit(training, spieler, anmeldung, tr) {	
 	//frappe.msgprint(spieler);
 	if (tr.style.backgroundColor == "red") {
