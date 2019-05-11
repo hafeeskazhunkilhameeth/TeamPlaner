c97178a55fac957181235bdd9b693b172f543007
Joelios <joel.mesmer@libracore.com>
2019-05-11 23:39:03 +0200

    preparing #66

--- teamplaner/www/teamverwaltung.js
+++ teamplaner/www/teamverwaltung.js
@@ -13,3 +13,71 @@ function reset_tab_team() {
 	document.getElementById("spieler_bearbeiten").style.display = "none";
 	document.getElementById("neuer_spieler").style.display = "none";
 }
+
+function show_neues_training() {
+	document.getElementById("training_section").style.display = "none";
+	document.getElementById("neues_training").style.display = "block";
+}
+
+function show_training_bearbeiten() {
+	document.getElementById("training_section").style.display = "none";
+	document.getElementById("training_bearbeiten").style.display = "block";
+}
+
+function reset_tab_training() {
+	document.getElementById("training_section").style.display = "block";
+	document.getElementById("neues_training").style.display = "none";
+	document.getElementById("training_bearbeiten").style.display = "none";
+}
+
+function show_neues_spiel() {
+	document.getElementById("spiel_section").style.display = "none";
+	document.getElementById("neues_spiel").style.display = "block";
+}
+
+function show_spiel_bearbeiten() {
+	document.getElementById("spiel_section").style.display = "none";
+	document.getElementById("spiel_bearbeiten").style.display = "block";
+}
+
+function reset_tab_spiel() {
+	document.getElementById("spiel_section").style.display = "block";
+	document.getElementById("neues_spiel").style.display = "none";
+	document.getElementById("spiel_bearbeiten").style.display = "none";
+}
+
+function add_spieler() {
+	
+}
+
+function add_training() {
+	
+}
+
+function add_spiel() {
+	
+}
+
+function change_spieler() {
+	
+}
+
+function change_training() {
+	
+}
+
+function change_spiel() {
+	
+}
+
+function delete_spieler() {
+	
+}
+
+function delete_training() {
+	
+}
+
+function delete_spiel() {
+	
+}
